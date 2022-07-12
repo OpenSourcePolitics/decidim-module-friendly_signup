95b91474ff95d4e50e9cb52613713ef077c0f353
Ivan Vergés <ivan@pokecode.net>
2022-07-12 13:08:40 +0200

    add github helpers

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Decidim::FriendlySignup
 
+[![[CI] Test](https://github.com/OpenSourcePolitics/decidim-module-friendly_signup/actions/workflows/test.yml/badge.svg)](https://github.com/OpenSourcePolitics/decidim-module-friendly_signup/actions/workflows/test.yml)
+[![Maintainability](https://api.codeclimate.com/v1/badges/46c261f70f7f49a8f385/maintainability)](https://codeclimate.com/github/OpenSourcePolitics/decidim-module-friendly_signup/maintainability)
+[![Test Coverage](https://codecov.io/gh/OpenSourcePolitics/decidim-module-friendly_signup/branch/master/graph/badge.svg?token=)](https://codecov.io/gh/OpenSourcePolitics/decidim-module-friendly_signup)
+
 A more user friendly approach for the user registration process.
 
 ## Usage
---
+++ spec/lib/overrides_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+# We make sure that the checksum of the file overriden is the same
+# as the expected. If this test fails, it means that the overriden
+# file should be updated to match any change/bug fix introduced in the core
+checksums = [
+  {
+    package: "decidim-core",
+    files: {
+      # "/app/commands/decidim/create_omniauth_registration.rb" => "10fc028fa6a7bef9936e6f9edb9b167b"
+    }
+  }
+]
+
+describe "Overriden files", type: :view do
+  checksums.each do |item|
+    # rubocop:disable Rails/DynamicFindBy
+    spec = ::Gem::Specification.find_by_name(item[:package])
+    # rubocop:enable Rails/DynamicFindBy
+    item[:files].each do |file, signature|
+      it "#{spec.gem_dir}#{file} matches checksum" do
+        expect(md5("#{spec.gem_dir}#{file}")).to eq(signature)
+      end
+    end
+  end
+
+  private
+
+  def md5(file)
+    Digest::MD5.hexdigest(File.read(file))
+  end
+end
