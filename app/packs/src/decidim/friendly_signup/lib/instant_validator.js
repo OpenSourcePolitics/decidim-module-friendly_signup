4b630373ae9b4e8ca817f1c1127f18824776c068
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2023-02-03 15:09:49 +0100

    Remove unnecessary console log

--- app/packs/src/decidim/friendly_signup/lib/instant_validator.js
+++ app/packs/src/decidim/friendly_signup/lib/instant_validator.js
@@ -77,7 +77,6 @@ export default class InstantValidator {
   }
 
   addErrors($dest, msg) {
-    console.log("$dest", $dest, "%form", this.$form)
     if ($dest.closest("label").find(".form-error").length > 1) {
       // Decidim may add and additional error class that does not play well with abide
       $dest.closest("label").find(".form-error:last").remove();
