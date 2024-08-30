2483fffbb145592b09f36b94a9066552ef5dd34b
barbara oliveira <barbarafictiovore@gmail.com>
2024-08-30 15:50:59 +0200

    Optimize the regex for better performance

--- app/forms/concerns/decidim/friendly_signup/registration_form_override.rb
+++ app/forms/concerns/decidim/friendly_signup/registration_form_override.rb
@@ -7,8 +7,9 @@ module Decidim
 
       private
 
+      EMAIL_REGEX = /\A[^<>"']+@[a-zA-Z0-9\-.]+\.[a-zA-Z]{2,}/
       def no_special_characters_in_email
-        errors.add(:email, :invalid) if email =~ /[<>'"]/
+        errors.add(:email, :invalid) if email =~ EMAIL_REGEX
       end
     end
   end
