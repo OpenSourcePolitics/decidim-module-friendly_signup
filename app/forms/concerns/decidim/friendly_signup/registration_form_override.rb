e4e9729cbff7b89d59ab08588a371861dc6586a6
barbara oliveira <barbarafictiovore@gmail.com>
2024-08-30 16:13:02 +0200

    lint

--- app/forms/concerns/decidim/friendly_signup/registration_form_override.rb
+++ app/forms/concerns/decidim/friendly_signup/registration_form_override.rb
@@ -8,6 +8,7 @@ module Decidim
       private
 
       EMAIL_REGEX = /\A[^<>"']+@[a-zA-Z0-9\-.]+\.[a-zA-Z]{2,}/
+
       def no_special_characters_in_email
         errors.add(:email, :invalid) if email =~ EMAIL_REGEX
       end
